f52ed9519e278daa792d8ed83429003b40fda956
Lisaa <lisaschool8classssss@gmail.com>
2023-08-28 14:20:10 +0300

    added listening events hashchange and load

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,11 +30,13 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcae5'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const data = await res.json();
@@ -159,4 +161,5 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
+
+['hashchange', 'load'].forEach(el => window.addEventListener(el, showRecipe));
