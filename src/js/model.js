1d80b2aa1f77a59d4d429dff4386d1c524445ff3
Lisaa <lisaschool8classssss@gmail.com>
2023-08-30 16:43:08 +0300

    refactoring code to using helpers and creating configuration file

--- src/js/model.js
+++ src/js/model.js
@@ -1,15 +1,13 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
-  const res = await fetch(
-    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-  );
-
-  const data = await res.json();
   try {
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
     const { recipe } = data.data;
 
     state.recipe = {
@@ -24,6 +22,7 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    // alert(err);
+    console.log(err);
   }
 };
